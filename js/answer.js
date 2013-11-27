6a8fac4c121b545953481279a0c01a97224a7595
Nina Dang <ninadangt@gmail.com>
2013-11-27 10:12:37 -0800

    Error fixed!!!!

--- js/answer.js
+++ js/answer.js
@@ -4,7 +4,7 @@ $(function() {
         alert('That is correct! Please circle PASSED on your sheet and proceed to the next page.');
     });    
 
-    $("#wrong").click(function(event) {
+    $(".wrong").click(function(event) {
         alert('Sorry, that is incorrect. Please mark a tally under the ATTEMPTS section and try again.');
     });  
 });
