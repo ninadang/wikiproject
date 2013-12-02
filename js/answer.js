841e3688cb93c9eca106f9c4585a908ee23aab62
Nina Dang <ninadangt@gmail.com>
2013-12-01 16:34:15 -0800

    Added features to support survey monkey

--- js/answer.js
+++ js/answer.js
@@ -1,11 +1,11 @@
 
 $(function() {
     $("#answer").click(function(event) {
-        alert('That is correct! Please circle PASSED on your sheet and proceed to the next page.');
+        alert('That is correct! Proceed to the next page.');
     });    
 
     $(".wrong").click(function(event) {
-        alert('Sorry, that is incorrect. Please mark a tally under the ATTEMPTS section and try again.');
+        alert('Sorry, that is incorrect. Please try again.');
     });  
 
 });
